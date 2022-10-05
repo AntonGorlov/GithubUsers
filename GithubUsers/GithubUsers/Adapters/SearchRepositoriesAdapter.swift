bfd18ea146ddb738c54fc52b8f213805cfeab6ab
Anton Gorlov <ta7oshka@gmail.com>
2022-10-05 17:03:59 +0300

    I have added implementation for "SearchRepositoriesAdapter"

--- GithubUsers/GithubUsers/Adapters/SearchRepositoriesAdapter.swift
+++ GithubUsers/GithubUsers/Adapters/SearchRepositoriesAdapter.swift
@@ -10,6 +10,34 @@ import Combine
 import BackendAPI
 
 /// Repositories adapter stub
-//class SearchRepositoriesAdapter: ISearchRepositoriesAdapter {
+class SearchRepositoriesAdapter: ISearchRepositoriesAdapter {
+    
+    var searchAPI: IGithubSearchAPI = GithubSearchAPIController()
+    
+    func getSearchRepositoriesWithSortAndStars(requestData: SearchRepositoryRequestData) -> GetSearchRepositoriesResult {
+        
+        return Future { promise in
+            
+            self.searchAPI.getSearchRepositories(with: requestData) { result in
+                
+                switch result {
+                    
+                case .success(let response):
+                    
+                    if !response.items.isEmpty {
+                        
+                        promise(.success(response))
+                        
+                    }
+                    promise(.failure(.requestExecutionError(.dataIsEmpty)))
+                    
+                case .failure(let error):
+                    
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
 
-//}
